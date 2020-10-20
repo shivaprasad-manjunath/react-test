3f88e503e29a07559422b5dca85204daba54d881
Shivaprasad <connect2shiv@gmail.com>
2020-10-20 20:13:53 +0530

    Intial Commit

---
+++ src/components/App.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from './App';
+
+const app = shallow(<App />);
+
+it('renders correctly', () => {
+    expect(app).toMatchSnapshot();
+});
+
+it('initializes the `state` with an empty list of gifts', () => {
+    expect(app.state().gifts).toEqual([]);
+});
+
+it('adds a new gift to `state` when clicking on `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.state().gifts).toEqual([{ id: 1 }]);
+});
+
+it('adds a new gift to the rendered list when clicking on `add gift` button', () => {
+    app.find('.btn-add').simulate('click');
+    expect(app.find('.gift-list').children().length).toEqual(2);
+});
